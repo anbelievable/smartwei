8185d4970afce1f74a5ed340844c194ce9b5e5d7
anbelievable <yonganwei91@163.com>
2021-07-04 11:51:11 +0800

    cc.h

---
+++ include/cc.h
@@ -0,0 +1,22 @@
+#ifndf _CC_H_
+#DEFINE _CC_H_
+
+class Person
+{
+public:
+	Person(){}
+	void speak();
+	void eat();
+	void sleep();
+	
+	
+	
+private:
+	int age;
+	string name;
+	
+	
+};
+
+
+#endif
